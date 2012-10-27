e951d753cafe6465a283630ad2e3e1d8f41d18aa
Daniel Ryding <decka.dee@gmail.com>
2012-10-27 12:51:35 +1100

    Removed remaining references to the Fake Repositories.

--- Caddy.ConsoleTest/Program.cs
+++ Caddy.ConsoleTest/Program.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using System.Net;
 using Caddy.Domain;
 using Caddy.Domain.Abstract;
-using Caddy.Domain.Concrete;
 
 namespace Caddy.ConsoleTest
 {
@@ -14,13 +13,6 @@ namespace Caddy.ConsoleTest
     {
         static void Main(string[] args)
         {
-            IPurchasesRepository PurchasesRepository = new FakePurchasesRepository(); // TODO: Wire up with IoC.
-            
-            foreach (var x in PurchasesRepository.Purchases)
-            {
-                Console.WriteLine("FakePurchase ID: {0}, Description: {1}.", x.PurchaseID, x.Desription);
-            }
-            Console.ReadKey();
         }
     }
 }
